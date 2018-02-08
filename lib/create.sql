1f4011376f5da371707ac083dc4d7f86ef8e1108
Rauof M <abdelrauof197@gmail.com>
2018-02-08 01:47:32 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id INTEGER primary key, name TEXT, age INTEGER, gender WHERE gender == "M" || gender == "F", color TEXT, alive BOOLEAN)
+CREATE TABLE bears (id INTEGER primary key, name TEXT, age INTEGER, gender WHERE gender == "M" || gender == "F", color TEXT, alive BIT)
