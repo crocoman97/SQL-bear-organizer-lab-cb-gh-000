547886c42eb9718223ce76f23fc3879127c6df9a
Rauof M <abdelrauof197@gmail.com>
2018-02-08 01:49:47 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,2 @@
-CREATE TABLE bears (id INTEGER primary key, name TEXT, age INTEGER, gender char(1), color TEXT, alive BIT);
+CREATE TABLE bears (id INTEGER primary key, name TEXT, age INTEGER, 
+gender char(1), color TEXT, alive BIT);
